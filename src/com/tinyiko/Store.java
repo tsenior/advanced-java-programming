ab2f8a2d1ccda2edb45f9bce3ee119b5932b0bb7
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-12 10:24:18 +0200

    refactor code

--- src/com/tinyiko/Store.java
+++ src/com/tinyiko/Store.java
@@ -14,7 +14,6 @@ public class Store {
         retrievePhoneNumbersFromNameMapUsingNameAsKey();
         retrieveNamesFromPhoneNumberMapUsingPhoneNumberAsKey();
         removeNameUsingKey();
-
         clearHashMaps();
 
     }
