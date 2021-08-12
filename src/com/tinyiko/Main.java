096fa4907547d5981b1c0f742f464be913118ca3
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-12 08:03:35 +0200

    implement a queue with linkedList

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,32 @@
 package com.tinyiko;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        LinkedList<String> characters = new LinkedList<>();
+
+
+        characters.add("C");
+        characters.add("Z");
+        characters.add(1,"B");
+        characters.addLast("D");
+        characters.addFirst("A");
+
+        System.out.println(characters);
+
+        characters.remove("Z");
+        System.out.println(characters);
+
+        if (!characters.contains("Z") ) {
+            System.out.println("Z doesnt exists");
+        }
+
+    //A queue is a first in first out data structure
+
+
     }
 }
